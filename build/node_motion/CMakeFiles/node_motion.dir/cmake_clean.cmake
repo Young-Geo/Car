6ae4968721633bf3a07c367f08e333eaa2033fc4
Young.Geo <anxan524@126.com>
2017-11-01 05:09:11 +0000

    修改motion

---
+++ build/node_motion/CMakeFiles/node_motion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node_motion.dir/src/node_motion.cpp.o"
+  "/home/pi/Car/devel/lib/node_motion/node_motion.pdb"
+  "/home/pi/Car/devel/lib/node_motion/node_motion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_motion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/node_motion/src/node_motion.cpp
+++ src/node_motion/src/node_motion.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <time.h>
 #include <wiringPi.h>
 #include <softPwm.h>
 
@@ -16,19 +19,19 @@
 #define PWMMAX 500
 
 
-void        motion_call(node_motion::motion & motionmsg)
+void        motion_call(const node_motion::motion::ConstPtr & motionmsg)
 {
     // if
-    ROS_INFO("LF_FORWARD PWM = %d\n", motionmsg.lf_forward);
-    ROS_INFO("RI_FORWARD PWM = %d\n", motionmsg.ri_forward);
-    ROS_INFO("LF_BACK PWM = %d\n", motionmsg.lf_back);
-    ROS_INFO("RI_BACK PWM = %d\n", motionmsg.ri_back);
+    ROS_INFO("LF_FORWARD PWM = %d\n", motionmsg->lf_forward);
+    ROS_INFO("RI_FORWARD PWM = %d\n", motionmsg->ri_forward);
+    ROS_INFO("LF_BACK PWM = %d\n", motionmsg->lf_back);
+    ROS_INFO("RI_BACK PWM = %d\n", motionmsg->ri_back);
 
     //
-    softPwmWrite(PINONE, motionmsg.lf_forward); //1
-    softPwmWrite(PINTWO, motionmsg.ri_forward); //4
-    softPwmWrite(PINTHREE, motionmsg.lf_back); //5
-    softPwmWrite(PINFOUR, motionmsg.ri_back); //6
+    softPwmWrite(PINONE, motionmsg->lf_forward); //1
+    softPwmWrite(PINTWO, motionmsg->ri_forward); //4
+    softPwmWrite(PINTHREE, motionmsg->lf_back); //5
+    softPwmWrite(PINFOUR, motionmsg->ri_back); //6
     //
 }
 
@@ -51,12 +54,12 @@ void        motionPiinit()
 
 int         main(int argc, const char **argv)
 {
-    ros::init(argc, (char **)argc, "node_motion");
+    ros::init(argc, (char **)argv, "node_motion");
 
     motionPiinit();
 
-    ros::nodeHandle n;
-    ros::Subscriber sub = n.subscribe("motion", 1000,motion_call);
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("motion", 1000, motion_call);
 
 
     ros::spin();
